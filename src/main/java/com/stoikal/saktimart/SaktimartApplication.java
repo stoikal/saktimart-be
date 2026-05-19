a3f8b41adee96bd812d778d73e72f6a737ca0f9b
Laksito Pamilih <laksito@protonmail.com>
2026-05-19 21:07:32 +0700

    initialize

---
+++ src/main/java/com/stoikal/saktimart/SaktimartApplication.java
@@ -0,0 +1,13 @@
+package com.stoikal.saktimart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaktimartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaktimartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stoikal/saktimart/SaktimartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stoikal.saktimart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaktimartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
