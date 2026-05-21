9152df05fcbc233ed67e73d437122658e1b7771b
Laksito Pamilih <laksito@protonmail.com>
2026-05-21 19:34:26 +0700

    chore: add env

--- src/main/java/com/stoikal/saktimart/controller/HelloController.java
+++ src/main/java/com/stoikal/saktimart/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.stoikal.saktimart.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +10,13 @@ import com.stoikal.saktimart.dto.HelloResponse;
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
-    
+
+    @Value("${POSTGRES_USERNAME}")
+    private String dbUser;
+
     @GetMapping("")
     public String sayHello() {
-        return "Hello, World!";
+        return "Hello, " + dbUser + "!";
     }
 
     @GetMapping("/json")
