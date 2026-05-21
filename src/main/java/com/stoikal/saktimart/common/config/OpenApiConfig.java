46ffce266c0017a7a0416d77032789e15a529ca3
Laksito Pamilih <laksito@protonmail.com>
2026-05-21 22:38:57 +0700

    chore: swagger

---
+++ src/main/java/com/stoikal/saktimart/common/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.stoikal.saktimart.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Saktimart API")
+                        .version("1.0")
+                        .description("Saktimart backend API"));
+    }
+}
--- src/main/java/com/stoikal/saktimart/productcategory/ProductCategoryController.java
+++ src/main/java/com/stoikal/saktimart/productcategory/ProductCategoryController.java
@@ -10,9 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.stoikal.saktimart.common.dto.ApiResponse;
 import com.stoikal.saktimart.productcategory.dto.CreateProductCategoryRequest;
 import com.stoikal.saktimart.productcategory.dto.ProductCategoryResponse;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@Tag(name = "Product Categories", description = "CRUD for product categories")
 @RestController
 @RequestMapping("/api/product-categories")
 public class ProductCategoryController {
